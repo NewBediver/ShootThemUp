f046387fc378f1d89fc30eba171d7f75d82515d6
Vladislav Serikov <SerikovVA@outlook.com>
2021-11-16 21:14:14 +0300

    Create Character and PlayerController. Setup camera.

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -1,12 +1,10 @@
 // Shoot Them Up Game. All Right Reserved.
 
 #include "STUGameModeBase.h"
+#include "Player/STUBaseCharacter.h"
+#include "Player/STUPlayerController.h"
 
-void ASTUGameModeBase::BeginPlay() {
-    int32 variable = 0;
-    if (variable < 10) {
-        for (int32 i = 0; i < 10; ++i) {
-            UE_LOG(LogTemp, Display, TEXT("index: %i"), i);
-        }
-    }
+ASTUGameModeBase::ASTUGameModeBase() {
+    DefaultPawnClass = ASTUBaseCharacter::StaticClass();
+    PlayerControllerClass = ASTUPlayerController::StaticClass();
 }
