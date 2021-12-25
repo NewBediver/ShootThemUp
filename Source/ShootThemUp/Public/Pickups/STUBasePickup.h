e810879a2238e415f9ae12e2f752547a4c9d47d9
Vladislav Serikov <SerikovVA@outlook.com>
2021-12-25 22:07:53 +0300

    Add base pickups.

---
+++ Source/ShootThemUp/Public/Pickups/STUBasePickup.h
@@ -0,0 +1,27 @@
+// Shoot Them Up Game. All Right Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUBasePickup.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUBasePickup : public AActor {
+    GENERATED_BODY()
+
+  public:
+    ASTUBasePickup();
+
+  protected:
+    virtual void BeginPlay() override;
+    virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+    UPROPERTY(VisibleAnywhere, Category = "Pickup")
+    USphereComponent* CollisionComponent = nullptr;
+
+  public:
+    virtual void Tick(float DeltaTime) override;
+};
