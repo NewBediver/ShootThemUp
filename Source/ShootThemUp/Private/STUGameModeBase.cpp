2c423f8c743b8e63b3a2880dc091e4e0295c7d62
Vladislav Serikov <SerikovVA@outlook.com>
2022-01-17 22:28:43 +0300

    Setup AI characters.

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -4,9 +4,39 @@
 #include "Player/STUBaseCharacter.h"
 #include "Player/STUPlayerController.h"
 #include "UI/STUGameHUD.h"
+#include "AIController.h"
 
 ASTUGameModeBase::ASTUGameModeBase() {
     DefaultPawnClass = ASTUBaseCharacter::StaticClass();
     PlayerControllerClass = ASTUPlayerController::StaticClass();
     HUDClass = ASTUGameHUD::StaticClass();
+}
+
+void ASTUGameModeBase::StartPlay() {
+    Super::StartPlay();
+
+    SpawnBots();
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(
+    AController* InController) {
+    if (InController != nullptr &&
+        InController->IsA<AAIController>()) {
+        return AIPawnClass;
+    }
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASTUGameModeBase::SpawnBots() {
+    if (GetWorld() == nullptr) {
+        return;
+    }
+
+    for (int32 i = 0; i < GameData.PlayersNum - 1; ++i) {
+        FActorSpawnParameters spawn_info;
+        spawn_info.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+        const auto stu_ai_controller = GetWorld()->SpawnActor<AAIController>(AIControllerClass, spawn_info);
+        RestartPlayer(stu_ai_controller);
+    }
 }
--- Source/ShootThemUp/Public/STUGameModeBase.h
+++ Source/ShootThemUp/Public/STUGameModeBase.h
@@ -4,15 +4,31 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "STUCoreTypes.h"
 #include "STUGameModeBase.generated.h"
 
-/**
- *
- */
+class AAIController;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase {
     GENERATED_BODY()
 
   public:
     explicit ASTUGameModeBase();
+
+    void StartPlay() override;
+    UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+  protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<AAIController> AIControllerClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<APawn> AIPawnClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    FGameData GameData;
+
+  private:
+    void SpawnBots();
 };
